f49c3609e48113ad0cddb0764e2959713055630b
Samuel Mburu <sammxuser@gmail.com>
2024-04-13 13:18:45 +0300

    complete project

--- script.js
+++ script.js
@@ -1,9 +1,40 @@
-const containerDiv = document.querySelector('#container');
+const container = document.querySelector('.container');
+const grid = document.querySelector('.grid');
+const button = document.querySelector('button');
+const buttonDiv = document.querySelector('.button');
 
-for (let i = 1; i <= 16; i++) {
-  for (let y = 1; y <= 16; y++) {
-    const squareDiv = document.createElement('div');
-    squareDiv.textContent = i * y;
-    containerDiv.appendChild(squareDiv);
+let numberOfSquares = '';
+
+button.addEventListener('click', () => {
+  numberOfSquares = prompt('Enter number of squares per side(Max 100)');
+  if (numberOfSquares !== null) {
+    if (numberOfSquares > 100) {
+      const errorParagraph = document.createElement('p');
+      errorParagraph.textContent =
+        'Error. Number of squares(' +
+        numberOfSquares +
+        ') entered is greater than 100. Try again';
+      errorParagraph.classList.add('error');
+      container.appendChild(errorParagraph);
+    } else if (numberOfSquares > 0) {
+      drawGrid(numberOfSquares);
+    }
+  }
+});
+
+function drawGrid(squares) {
+  grid.replaceChildren(); // Clear the grid before drawing again
+  for (let i = 1; i <= squares; i++) {
+    for (let y = 1; y <= squares; y++) {
+      const gridItem = document.createElement('div');
+      gridItem.classList.add('grid-item');
+      gridItem.addEventListener('mouseover', (event) => {
+        event.target.style.backgroundColor = 'salmon';
+      });
+      gridItem.addEventListener('mouseout', (event) => {
+        event.target.style.backgroundColor = 'green';
+      });
+      grid.appendChild(gridItem);
+    }
   }
 }
